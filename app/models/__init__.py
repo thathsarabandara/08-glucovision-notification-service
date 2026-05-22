394778512fd8f3bfad2e0e7d2097b9935218dc68
Thathsara Bandara <thathsaraarumapperuma@gmail.com>
2026-05-22 17:09:36 +0530

    feat: implement SQLAlchemy database models for notification management system

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from .base import Base
+from .notification import Notification
+from .notification_template import NotificationTemplate
+from .notification_log import NotificationLog
+from .scheduled_notification import ScheduledNotification
+from .user_notification_preference import UserNotificationPreferenceCache
